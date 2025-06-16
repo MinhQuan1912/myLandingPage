7531c0ab3353815f2ec5ab2c30acaf98b33bfed4
hoangtl <hoangtl@famtech.net>
2025-06-16 17:07:45 +0700

    fix theme

---
+++ app.config.ts
@@ -0,0 +1,20 @@
+export default defineAppConfig({
+  // https://ui.nuxt.com/getting-started/theme#design-system
+  ui: {
+    colors: {
+      primary: "emerald",
+      neutral: "slate",
+    },
+    ui: {
+      notifications: {
+        position: "top-0 right-0",
+      },
+    },
+    button: {
+      defaultVariants: {
+        // Set default button color to neutral
+        // color: 'neutral'
+      },
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <UButton label="Hiện toast" @click="toast.add({ title: 'Test' })" />
+    <h1 class="text-customRed-900">title</h1>
+  </div>
+</template>
+<script setup lang="ts">
+const toast = useToast();
+function showToast() {
+  toast.add({
+    title: "Thông báo thành công",
+  });
+}
+</script>
+<style></style>
